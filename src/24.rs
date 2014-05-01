6038a9765f7498b66d48f0b8cddcea81191aee71
Renato Zannon <renato@rrsz.com.br>
2014-05-01 18:14:30 -0300

    Remove dangling 'm'

--- src/24.rs
+++ src/24.rs
@@ -1,7 +1,7 @@
 /* Problem 24: Lexicographic permutations
  *
  * A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation
-m* of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically,
+ * of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically,
  * we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
  *
  * 012   021   102   120   201   210
