af2106c023c4ca5c327ada045fb809ef60917236
Renato Zannon <renato@rrsz.com.br>
2014-05-25 01:28:36 -0300

    Solved problem 36
    ...In 5 minutes. Was easier than I thought :)
    Knowing the standard library makes a LOT of difference.

---
+++ src/36.rs
@@ -0,0 +1,26 @@
+/* Problem 36: Double-base palindromes
+ * 
+ * The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+ * 
+ * Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+ * 
+ * (Please note that the palindromic number, in either base, may not include leading zeros.) */
+
+use std::iter::AdditiveIterator;
+use std::num::ToStrRadix;
+
+fn main() {
+  let result = range(1, 1_000_000).filter(|number| {
+    is_palindrome(number.to_str()) && is_palindrome(number.to_str_radix(2))
+  }).sum();
+
+  println!("{}", result);
+}
+
+fn is_palindrome(string: StrBuf) -> bool {
+  let slice = string.as_slice();
+
+  slice.chars().zip(slice.chars().rev()).all(|(from_start, from_end)| {
+    from_start == from_end
+  })
+}
