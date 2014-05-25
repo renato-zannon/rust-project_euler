6c02b06e6dba280fe0c4a3bec04c49be6a69c157
Renato Zannon <renato@rrsz.com.br>
2014-05-25 01:00:35 -0300

    Solve problem 35

---
+++ src/35.rs
@@ -0,0 +1,48 @@
+/* Problem 35: Circular primes
+ *
+ * The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and
+ * 719, are themselves prime.
+ *
+ * There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+ *
+ * How many circular primes are there below one million? */
+
+extern crate shared;
+use shared::{digits, sieve};
+
+fn main() {
+  let mut sieve = sieve::new();
+  let search_space: Vec<uint> = sieve.by_ref().take_while(|&prime| prime < 1_000_000).collect();
+
+  let result = search_space.move_iter().count(|prime| {
+    is_circular(prime, &mut sieve)
+  });
+
+  println!("{}", result);
+}
+
+fn is_circular(prime: uint, sieve: &mut sieve::Sieve) -> bool {
+  rotations_of(prime).move_iter().all(|rotation| {
+    sieve.is_prime(rotation)
+  })
+}
+
+fn rotations_of(number: uint) -> Vec<uint> {
+  use std::num::pow;
+
+  let number_len = digits::new(number).len();
+
+  let biggest_unit = pow(10u, number_len - 1);
+
+  range(0, number_len).scan(number, |state, _| {
+    let prev_rotation = *state;
+
+    let head = prev_rotation % 10;
+    let tail = prev_rotation / 10;
+
+    let rotation = head * biggest_unit + tail;
+    *state = rotation;
+
+    Some(rotation)
+  }).collect()
+}
