14c27f0b234857d7102571d38587e283aa9ebafc
Renato Zannon <renato@rrsz.com.br>
2014-05-25 10:56:37 -0300

    Solve problem 37

---
+++ src/37.rs
@@ -0,0 +1,59 @@
+/* Problem 37: Truncatable primes
+ *
+ * The number 3797 has an interesting property. Being prime itself, it is possible to continuously
+ * remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly
+ * we can work from right to left: 3797, 379, 37, and 3.
+ *
+ * Find the sum of the only eleven primes that are both truncatable from left to right and right to
+ * left.
+ *
+ * NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes. */
+
+extern crate shared;
+use shared::{digits, sieve};
+use std::iter::AdditiveIterator;
+
+fn main() {
+  let search_space = sieve::new().skip_while(|&prime| prime < 10);
+
+  let mut prime_checker = sieve::new();
+
+  let mut truncatable_primes = search_space
+    .filter(|&prime| is_truncatable(prime, &mut prime_checker))
+    .take(11);
+
+  println!("{}", truncatable_primes.sum());
+}
+
+fn is_truncatable(prime: uint, sieve: &mut sieve::Sieve) -> bool {
+  truncatable_from_left(prime, sieve) &&
+    truncatable_from_right(prime, sieve)
+}
+
+fn truncatable_from_left(prime: uint, sieve: &mut sieve::Sieve) -> bool {
+  let prime_digits: Vec<uint> = digits::new(prime).collect();
+
+  prime_digits.iter().rev().scan((0, 1), |state, &digit| {
+    let (previous, multiplier) = *state;
+    let truncation = previous + digit * multiplier;
+
+    *state = (truncation, multiplier * 10);
+    Some(truncation)
+  }).all(|truncation| {
+    sieve.is_prime(truncation)
+  })
+}
+
+fn truncatable_from_right(prime: uint, sieve: &mut sieve::Sieve) -> bool {
+  let mut remaining = prime / 10;
+
+  while remaining > 0 {
+    if !sieve.is_prime(remaining) {
+      return false;
+    }
+
+    remaining = remaining / 10;
+  }
+
+  return true;
+}
