c189fad2bfb1e3639352835c578958b65bf3f051
Renato Zannon <renato@rrsz.com.br>
2014-06-07 22:13:55 -0300

    Add alternative solution to problem 31

---
+++ src/31-2.rs
@@ -0,0 +1,36 @@
+/* Problem 31: Coin sums
+ *
+ * In England the currency is made up of pound, £, and pence, p, and there are eight coins in
+ * general circulation:
+ *
+ *     1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+ *
+ * It is possible to make £2 in the following way:
+ *
+ *     1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+ *
+ * How many different ways can £2 be made using any number of coins? */
+
+use std::iter::range_inclusive;
+
+static DENOMINATIONS: &'static [uint] = &[200, 100, 50, 20, 10, 5, 2, 1];
+
+fn main() {
+  println!("{}", ways_to_make(200, DENOMINATIONS));
+}
+
+// Dynamic programming solution, adapted from the overview PDF
+fn ways_to_make(value: uint, denominations: &[uint]) -> uint {
+  let mut ways = Vec::from_elem(value + 1, 0u);
+  let ways = ways.as_mut_slice();
+
+  ways[0] = 1;
+
+  for &denom in denominations.iter() {
+    for current in range_inclusive(denom, value) {
+      ways[current] += ways[current - denom];
+    }
+  }
+
+  *ways.last().unwrap()
+}
