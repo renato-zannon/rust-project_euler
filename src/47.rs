1db73b0552f61bfcaf5056a38a86a22ce014579b
Renato Zannon <renato@rrsz.com.br>
2014-06-08 05:02:12 -0300

    Solve problem 47

---
+++ src/47.rs
@@ -0,0 +1,85 @@
+/* Problem 47: Distinct primes factors
+ * 
+ * The first two consecutive numbers to have two distinct prime factors are:
+ * 
+ * 14 = 2 × 7
+ * 15 = 3 × 5
+ * 
+ * The first three consecutive numbers to have three distinct prime factors are:
+ * 
+ * 644 = 2² × 7 × 23
+ * 645 = 3 × 5 × 43
+ * 646 = 2 × 17 × 19.
+ * 
+ * Find the first four consecutive integers to have four distinct prime factors. What is the first
+ * of these numbers? */
+
+extern crate shared;
+
+use shared::sieve;
+use std::iter::count;
+use std::collections::{HashMap, HashSet};
+
+static CONSECUTIVE_COUNT: uint = 4;
+
+fn main() {
+  let mut sieve = sieve::new();
+  let mut memo  = HashMap::new();
+
+  let result = count(1u, 1).find(|&first_number| {
+    range(first_number, first_number + CONSECUTIVE_COUNT).all(|number| {
+      sieve.compute_until(number);
+
+      let count = factors_for_number(FactorCount {
+        remaining: number,
+        memo:      &mut memo,
+        primes:    sieve.found_primes(),
+      });
+
+      count == CONSECUTIVE_COUNT
+    })
+  }).unwrap();
+
+  println!("{}", result);
+}
+
+type Memo<'a> = &'a mut HashMap<uint, HashSet<uint>>;
+
+fn factors_for_number(count: FactorCount) -> uint {
+  let (_, result) = factors(count);
+  result.len()
+}
+
+struct FactorCount<'a> {
+  remaining: uint,
+  memo:      Memo<'a>,
+  primes:    &'a [uint]
+}
+
+fn factors<'a>(mut count: FactorCount<'a>) -> (Memo<'a>, HashSet<uint>) {
+  if count.memo.contains_key(&count.remaining) {
+    let cached = count.memo.get(&count.remaining).clone();
+    return (count.memo, cached);
+  }
+
+  let first_factor = count.primes.iter().find(|&&prime| {
+    count.remaining % prime == 0 
+  }).map(|value| *value);
+
+  let (new_memo, result) = match first_factor {
+    Some(factor) => {
+      let subcount = FactorCount { remaining: count.remaining / factor, ..count };
+      let (new_memo, mut result) = factors(subcount);
+
+      result.insert(factor);
+      (new_memo, result)
+    },
+
+    None => (count.memo, HashSet::new()),
+  };
+
+  count.memo = new_memo;
+  count.memo.insert(count.remaining, result.clone());
+
+  (count.memo, result)
+}
