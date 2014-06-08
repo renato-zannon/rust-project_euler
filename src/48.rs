359063bec225c198dae6f845219b383d3f63bc46
Renato Zannon <renato@rrsz.com.br>
2014-06-08 19:38:52 -0300

    Solve problem 48

---
+++ src/48.rs
@@ -0,0 +1,29 @@
+/* Problem 48: Self powers
+ *
+ * The series, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
+ *
+ * Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰ */
+
+use std::num::pow;
+use std::iter::range_inclusive;
+
+fn main() {
+  let ten_to_ten = pow(10u, 10u) as u64;
+  let take_10_digits = |number: u64| {
+    number % ten_to_ten
+  };
+
+  let mut result: u64 = 0;
+
+  for number in range_inclusive(1u64, 1000) {
+    let mut number_result: u64 = 1;
+
+    for _ in range(0u64, number) {
+      number_result = take_10_digits(number_result) * number;
+    }
+
+    result = take_10_digits(number_result) + take_10_digits(result);
+  }
+
+  println!("{}", take_10_digits(result));
+}
