e512671574cc80bafdb647bca2d4c008add0014e
Renato Zannon <renato@rrsz.com.br>
2014-06-17 01:30:56 -0300

    Solved problem 51
    Or so I think, because Project Euler is currently offline :(

---
+++ src/51.rs
@@ -0,0 +1,142 @@
+/* Problem 51: Prime digit replacements
+ *
+ * By replacing the 1st digit of the 2-digit number *3, it turns out that six of the nine possible
+ * values: 13, 23, 43, 53, 73, and 83, are all prime.
+ *
+ * By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit number is the
+ * first example having seven primes among the ten generated numbers, yielding the family: 56003,
+ * 56113, 56333, 56443, 56663, 56773, and 56993. Consequently 56003, being the first member of this
+ * family, is the smallest prime with this property.
+ *
+ * Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits)
+ * with the same digit, is part of an eight prime value family. */
+
+extern crate shared;
+use shared::{digits, sieve};
+
+fn main() {
+  let mut sieve = sieve::new();
+
+  loop {
+    let prime = sieve.next().unwrap();
+    let prime_digits = digits::new(prime).collect::<Vec<uint>>();
+
+    let mut families = range(1, prime_digits.len()).flat_map(|variable_count| {
+      families_from_variables(variable_count, &prime_digits).move_iter()
+    });
+
+    for family_iter in families {
+      let family = family_iter.filter(|&num| sieve.is_prime(num)).collect::<Vec<uint>>();
+
+      if family.len() == 8 {
+        println!("{}", prime);
+        return;
+      }
+    }
+  }
+}
+
+fn families_from_variables(count: uint, digits: &Vec<uint>) -> Vec<FamilyIterator> {
+  let mut result    = Vec::new();
+  let mut variables = Vec::from_fn(count, |index| index);
+
+  let digit_count = digits.len();
+
+  loop {
+    result.push(FamilyIterator {
+      variables: variables.clone(),
+      template:  digits.clone(),
+      last_used: None,
+    });
+
+    let mut max_index = digit_count - 1;
+    let mut var_slice = variables.as_mut_slice();
+
+    loop {
+      let last_ref = match var_slice.mut_pop_ref() {
+        None        => return result,
+        Some(value) => value,
+      };
+
+      let next_value = *last_ref + 1;
+
+      if next_value > max_index {
+        *last_ref = match var_slice.last() {
+          Some(prev_var) => prev_var + 2,
+          None           => return result,
+        };
+
+        max_index -= 1;
+      } else {
+        *last_ref = next_value;
+        break;
+      }
+    }
+  }
+}
+
+struct FamilyIterator {
+  variables: Vec<uint>,
+  template:  Vec<uint>,
+  last_used: Option<uint>,
+}
+
+impl Iterator<uint> for FamilyIterator {
+  fn next(&mut self) -> Option<uint> {
+    let last_used = match self.last_used {
+      val @ Some(_) => val,
+      None          => return self.init_pattern(),
+    };
+
+    match last_used {
+      None | Some(9) => None,
+
+      Some(prev) => {
+        let new   = prev + 1;
+        let templ = self.template.as_mut_slice();
+
+        for &var_index in self.variables.iter() {
+          templ[var_index] = new;
+        }
+        let new_value = FamilyIterator::to_number(templ);
+
+        self.last_used = Some(new);
+        Some(new_value)
+      }
+    }
+  }
+}
+
+impl FamilyIterator {
+  fn to_number(digits: &[uint]) -> uint {
+    digits.iter().fold(0, |acc, &digit| {
+      acc * 10 + digit
+    })
+  }
+
+  fn init_pattern(&mut self) -> Option<uint> {
+    let mut found_variable = None;
+    let templ = self.template.as_slice();
+
+    for &var_index in self.variables.iter() {
+      let on_template = match templ.get(var_index) {
+        Some(&value) => value,
+        None         => return None,
+      };
+
+      match found_variable {
+        None       => found_variable = Some(on_template),
+        Some(prev) => {
+          if prev != on_template {
+            return None;
+          }
+        }
+      }
+    }
+
+    self.last_used = found_variable;
+    found_variable.map(|_| {
+      FamilyIterator::to_number(self.template.as_slice())
+    })
+  }
+}
