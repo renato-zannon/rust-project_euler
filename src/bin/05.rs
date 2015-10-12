6a5735bf9e19f60198d5ac08a1c7beee95f3746e
Renato Zannon <renato@rrsz.com.br>
2015-10-11 22:40:01 -0300

    Actually implement problem 5's lcm

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -5,13 +5,25 @@
  *
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? */
 
-#![feature(core)]
-extern crate num;
-
-use std::iter::range_inclusive as irange;
-use num::integer::lcm;
+use std::cmp::Ordering;
 
 fn main() {
-    let n = irange(1u32, 20).fold(1, lcm);
+    let n = (1u64..21).fold(1, lcm);
     println!("{}", n);
+}
+
+fn lcm(n1: u64, n2: u64) -> u64 {
+    let mut n1_factor = 1;
+    let mut n2_factor = 1;
+
+    loop {
+        let n1_multiple = n1 * n1_factor;
+        let n2_multiple = n2 * n2_factor;
+
+        match n1_multiple.cmp(&n2_multiple) {
+            Ordering::Less    => n1_factor += 1,
+            Ordering::Greater => n2_factor += 1,
+            Ordering::Equal   => return n1_multiple,
+        }
+    }
 }
