b7410e9a1d2286441dae93c749017c8a6f12a3b1
Renato Zannon <renato@rrsz.com.br>
2015-10-11 21:51:55 -0300

    Move solution 04 from the combinations library

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,58 +5,65 @@
  *
  * Find the largest palindrome made from the product of two 3-digit numbers. */
 
-#![feature(core)]
-extern crate shared;
-
-use std::iter::range_inclusive;
-use std::iter::Unfold;
-
-use shared::combinations;
-
-fn main() {
-    let pairs = combinations::new(range_inclusive(100 as usize, 999).collect());
-
-    let (result, (n1, n2)) = pairs
-        .filter(|&(n1, n2): &(usize, usize)| n1 <= n2)
-        .filter_map(select_palindromes)
-        .max_by(|&(n, _)| n)
-        .unwrap();
-
-    println!("{} ({}, {})", result, n1, n2);
+struct Counter<'a> {
+    values: &'a [u32],
+    position: usize,
 }
 
-fn select_palindromes(pair: (usize, usize)) -> Option<(usize, (usize, usize))> {
-    let &(n1, n2) = &pair;
+impl<'a> Iterator for Counter<'a> {
+    type Item = (u32, u32);
+
+    fn next(&mut self) -> Option<(u32, u32)> {
+        let base = match self.values.get(0) {
+            Some(value) => value.clone(),
+            None => return None,
+        };
+
+        match self.values.get(self.position) {
+            Some(value) => {
+                self.position += 1;
+                Some((base, value.clone()))
+            },
+
+            None => {
+                self.position = 0;
+                self.values = & self.values[1..];
+                self.next()
+            }
+        }
+    }
+}
+
+fn main() {
+    let values: Vec<u32> = (100..1000).collect();
+    let pairs = Counter { values: &values, position: 0 };
+
+    let result = pairs
+        .filter_map(select_palindromes)
+        .max()
+        .unwrap();
+
+    println!("{}", result);
+}
+
+fn select_palindromes((n1, n2): (u32, u32)) -> Option<u32> {
     let mult = n1 * n2;
 
-    if is_palindrome(mult) {
-        Some((mult, (n1, n2)))
+    if mult == reverse(mult) {
+        Some(mult)
     } else {
         None
     }
 }
 
-fn is_palindrome(n: usize) -> bool {
-    n == reverse(n)
-}
+fn reverse(n: u32) -> u32 {
+    let mut remaining = n;
+    let mut reversed = 0;
 
-fn reverse(n: usize) -> usize {
-    return Unfold::new((n, 0), unfold_reverse).last().unwrap();
-
-    fn unfold_reverse(state_ptr: &mut (usize, usize)) -> Option<usize> {
-        let &mut (remaining, reversed) = state_ptr;
-
-        if remaining <= 0 {
-            return None;
-        }
-
-        let new_remaining = remaining / 10;
-        let div_remainder = remaining % 10;
-
-        let new_reversed = reversed * 10 + div_remainder;
-
-        *state_ptr = (new_remaining, new_reversed);
-
-        Some(new_reversed)
+    while remaining > 0 {
+        reversed = reversed * 10 + (remaining % 10);
+        remaining /= 10;
     }
+
+    reversed
 }
