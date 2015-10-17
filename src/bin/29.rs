6fafcca8e21c18a771054eacb4f2bc978f51dccd
Renato Zannon <renato@rrsz.com.br>
2015-10-17 18:20:58 -0300

    Minor warnings on problem 29

--- src/bin/29.rs
+++ src/bin/29.rs
@@ -1,12 +1,12 @@
 use std::collections::HashSet;
 
 fn main() {
-    let mut powers: HashSet<(usize, usize)> = HashSet::new();
+    let mut powers: HashSet<(u32, u32)> = HashSet::new();
 
-    for a in (2usize..101) {
+    for a in 2..101 {
         let (base_a, factor) = factor_for(a);
 
-        for b in (2usize..101) {
+        for b in 2..101 {
             powers.insert((base_a, factor * b));
         }
     }
@@ -14,13 +14,13 @@ fn main() {
     println!("{}", powers.len());
 }
 
-fn factor_for(num: usize) -> (usize, usize) {
+fn factor_for(num: u32) -> (u32, u32) {
     let fnum:       f64 = num as f64;
 
     let mut factor: f64  = 1.0;
-    let mut base:   usize = num;
+    let mut base:   u32 = num;
 
-    for potential_root in (2..num) {
+    for potential_root in 2..num {
         let exp = fnum.log(potential_root as f64);
 
         if exp.fract() == 0f64 {
@@ -30,5 +30,5 @@ fn factor_for(num: usize) -> (usize, usize) {
         }
     }
 
-    (base, factor as usize)
+    (base, factor as u32)
 }
