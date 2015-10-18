ba7e96ed9caa83623113da5aebe7b15513364011
Renato Zannon <renato@rrsz.com.br>
2015-10-18 02:25:21 -0200

    Fix problem 40

--- src/bin/40.rs
+++ src/bin/40.rs
@@ -11,28 +11,13 @@
  *
  * d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000 */
 
-#![feature(core)]
 extern crate shared;
 use shared::digits;
-use std::iter::{count, MultiplicativeIterator};
 
 fn main() {
-    let mut sequence = count(1usize, 1)
-        .flat_map(|number| digits::new::<usize, usize>(number))
-        .enumerate();
-
-    let mut get_digit = move |number: usize| {
-        let (_, digit) = sequence
-            .find(|&(idx, _)| idx == number - 1)
-            .unwrap();
-
-        digit
-    };
-
-    let result = [1, 10, 100, 1000, 10000, 100000, 1000000]
-        .iter()
-        .map(|&position| get_digit(position))
-        .product();
+    let result: u32 = [1, 10, 100, 1000, 10000, 100000, 1000000].iter().map(|&position| {
+        (1..).flat_map(digits::new::<_, u32>).nth(position as usize - 1).unwrap()
+    }).fold(1, |acc, n| acc * n);
 
     println!("{}", result);
 }
