bc98f923d7cc6ab216cfe8dc4625120df64cc1c7
Renato Zannon <renato@rrsz.com.br>
2015-10-17 23:48:25 -0300

    Fix problem 34

--- src/bin/34.rs
+++ src/bin/34.rs
@@ -6,32 +6,28 @@
  *
  * Note: as 1! = 1 and 2! = 2 are not sums they are not included. */
 
-#![feature(core)]
 extern crate shared;
-
-use std::iter::{range_inclusive, AdditiveIterator, count};
-use std::num::Int;
 use shared::digits;
 
 fn main() {
     let max_number = {
         let max_single_digit_factorial = factorial(9);
 
-        let max_digits = count(1usize, 1usize).find(|&digits| {
+        let max_digits = (1..).find(|&digits| {
             max_single_digit_factorial * digits < max_number_with_digits(digits)
         }).unwrap();
 
         max_single_digit_factorial * max_digits
     };
 
-    let result = (3usize..max_number).filter(|&number| {
+    let result = (3u32..max_number).filter(|&number| {
         number_eqls_fact_sum(number)
-    }).sum();
+    }).fold(0, |acc, num| acc + num);
 
     println!("{}", result);
 }
 
-fn number_eqls_fact_sum(number: usize) -> bool {
+fn number_eqls_fact_sum(number: u32) -> bool {
     let mut fact_sum = 0;
 
     for digit in digits::new(number) {
@@ -45,12 +41,12 @@ fn number_eqls_fact_sum(number: usize) -> bool {
     fact_sum == number
 }
 
-fn factorial(n: usize) -> usize {
-    range_inclusive(1, n).fold(1, |num, result| {
+fn factorial(n: u32) -> u32 {
+    (1..n + 1).fold(1, |num, result| {
         num * result
     })
 }
 
-fn max_number_with_digits(digit_count: usize) -> usize {
-    10usize.pow(digit_count) - 1
+fn max_number_with_digits(digit_count: u32) -> u32 {
+    10u32.pow(digit_count) - 1
 }
