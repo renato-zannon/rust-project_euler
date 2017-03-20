1d43eff81cfed65d73b3084b30e0652fb8d3bab4
Renato Zannon <renato@rrsz.com.br>
2017-03-19 22:55:58 -0300

    Fix problem 36

--- src/bin/36.rs
+++ src/bin/36.rs
@@ -6,23 +6,43 @@
  *
  * (Please note that the palindromic number, in either base, may not include leading zeros.) */
 
-#![feature(fmt_radix)]
 extern crate shared;
-use std::fmt::{self, Write};
+use std::fmt::Write;
+
+trait NumberFormatter {
+    fn format(number: u32, buffer: &mut String);
+}
+
+struct Binary;
+struct Decimal;
+
+impl NumberFormatter for Binary {
+    fn format(number: u32, buffer: &mut String) {
+        write!(buffer, "{:b}", number).unwrap();
+    }
+}
+
+impl NumberFormatter for Decimal {
+    fn format(number: u32, buffer: &mut String) {
+        write!(buffer, "{}", number).unwrap();
+    }
+}
 
 fn main() {
-    let mut buffer = String::with_capacity(50);
+    let mut b1 = String::with_capacity(50);
+    let mut b2 = String::with_capacity(50);
 
-    let result = (1..1_000_000).filter(|number| {
-        is_palindrome(*number, 10, &mut buffer) && is_palindrome(*number, 2, &mut buffer)
-    }).fold(0, |acc, num| acc + num);
+    let result: u32 = (1..1_000_000)
+        .filter(|number| is_palindrome::<Decimal>(*number, &mut b1))
+        .filter(|number| is_palindrome::<Binary>(*number, &mut b2))
+        .sum();
 
     println!("{}", result);
 }
 
-fn is_palindrome(number: u32, base: u8, buffer: &mut String) -> bool {
+fn is_palindrome<T: NumberFormatter>(number: u32, buffer: &mut String) -> bool {
     buffer.clear();
-    write!(buffer, "{}", fmt::radix(number, base)).unwrap();
+    T::format(number, buffer);
 
     buffer.chars().zip(buffer.chars().rev()).all(|(from_start, from_end)| {
         from_start == from_end
