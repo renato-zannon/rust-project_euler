bd394ce143dd438771a0c716ca29f1ea95bb53c4
Renato Zannon <renato@rrsz.com.br>
2019-06-17 12:15:41 -0300

    Refactor A* impl

--- shared/src/a_star.rs
+++ shared/src/a_star.rs
@@ -1,10 +1,10 @@
-use hashbrown::{HashMap, HashSet};
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 use std::hash::Hash;
 
+mod algorithm;
 pub mod matrix;
-pub use matrix::{Coord, Matrix};
+
+use self::algorithm::Algorithm;
+pub use self::matrix::{Coord, Matrix};
 
 pub trait Traversable {
     type Coord: Copy + Eq + Hash;
@@ -15,97 +15,7 @@ pub trait Traversable {
     fn reached_goal(&self, node: Self::Coord) -> bool;
 }
 
-struct ScoredCoord<C> {
-    coord: C,
-    score: u32,
-}
-
-impl<C> Ord for ScoredCoord<C> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        // inverted on purpose, since we want the coordinates with the lowest f-score first
-        other.score.cmp(&self.score)
-    }
-}
-
-impl<C> PartialOrd for ScoredCoord<C> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<C> PartialEq for ScoredCoord<C> {
-    fn eq(&self, other: &Self) -> bool {
-        self.score == other.score
-    }
-}
-
-impl<C> Eq for ScoredCoord<C> {}
-
-fn scored<C>(coord: C, score: u32) -> ScoredCoord<C> {
-    ScoredCoord { coord, score }
-}
-
 pub fn a_star<T: Traversable>(graph: &T, start: T::Coord) -> Vec<T::Coord> {
-    let mut closed_set = HashSet::new();
-
-    let mut open_set = HashSet::new();
-    open_set.insert(start);
-
-    let mut open_set_queue = BinaryHeap::new();
-    open_set_queue.push(scored(start, graph.heuristic(start)));
-
-    let mut came_from = HashMap::new();
-
-    let mut g_score = HashMap::new();
-    g_score.insert(start, 0);
-
-    while let Some(ScoredCoord { coord: current, .. }) = open_set_queue.pop() {
-        if graph.reached_goal(current) {
-            return reconstruct_path::<T>(&came_from, current);
-        }
-
-        open_set.remove(&current);
-        closed_set.insert(current);
-
-        for neighbor in graph.neighbors(current) {
-            if closed_set.contains(&neighbor) {
-                continue;
-            }
-
-            let tentative_gscore = get_score::<T>(&g_score, &current)
-                .saturating_add(graph.dist_between(current, neighbor));
-
-            if open_set.insert(neighbor) {
-                let fscore = tentative_gscore.saturating_add(graph.heuristic(neighbor));
-                open_set_queue.push(scored(neighbor, fscore));
-            } else if tentative_gscore >= get_score::<T>(&g_score, &neighbor) {
-                continue;
-            }
-
-            came_from.insert(neighbor, current);
-            g_score.insert(neighbor, tentative_gscore);
-        }
-    }
-
-    panic!("There's no path from start to goal");
-}
-
-fn get_score<T: Traversable>(scores: &HashMap<T::Coord, u32>, node: &T::Coord) -> u32 {
-    scores.get(node).cloned().unwrap_or(std::u32::MAX)
-}
-
-fn reconstruct_path<T: Traversable>(
-    came_from: &HashMap<T::Coord, T::Coord>,
-    start: T::Coord,
-) -> Vec<T::Coord> {
-    let mut path = vec![start];
-    let mut current = start;
-
-    while came_from.contains_key(&current) {
-        current = came_from[&current];
-        path.push(current);
-    }
-
-    path.reverse();
-    path
+    let astar = Algorithm::new(graph, start);
+    astar.perform()
 }
---
+++ shared/src/a_star/algorithm.rs
@@ -0,0 +1,128 @@
+use super::Traversable;
+use hashbrown::{HashMap, HashSet};
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+pub(super) struct Algorithm<'t, T: Traversable> {
+    graph: &'t T,
+    closed_set: HashSet<T::Coord>,
+    open_set: HashSet<T::Coord>,
+    open_set_queue: BinaryHeap<ScoredCoord<T::Coord>>,
+    came_from: HashMap<T::Coord, T::Coord>,
+    g_score: HashMap<T::Coord, u32>,
+}
+
+impl<'t, T: Traversable> Algorithm<'t, T> {
+    pub(super) fn new(graph: &'t T, start: T::Coord) -> Self {
+        let mut open_set = HashSet::new();
+        open_set.insert(start);
+
+        let mut open_set_queue = BinaryHeap::new();
+        open_set_queue.push(scored(start, graph.heuristic(start)));
+
+        let mut g_score = HashMap::new();
+        g_score.insert(start, 0);
+
+        Self {
+            graph,
+            open_set,
+            open_set_queue,
+            closed_set: HashSet::new(),
+            came_from: HashMap::new(),
+            g_score,
+        }
+    }
+
+    pub(super) fn perform(mut self) -> Vec<T::Coord> {
+        while let Some(current) = self.visit() {
+            if self.graph.reached_goal(current) {
+                return self.reconstruct_path(current);
+            }
+
+            for neighbor in self.graph.neighbors(current) {
+                if self.closed_set.contains(&neighbor) {
+                    continue;
+                }
+
+                let tentative_gscore = self
+                    .g_score(&current)
+                    .saturating_add(self.graph.dist_between(current, neighbor));
+
+                let discovered = self.discover(neighbor, tentative_gscore);
+
+                if discovered || tentative_gscore < self.g_score(&neighbor) {
+                    self.came_from.insert(neighbor, current);
+                    self.g_score.insert(neighbor, tentative_gscore);
+                }
+            }
+        }
+
+        panic!("There's no path from start to goal");
+    }
+
+    fn visit(&mut self) -> Option<T::Coord> {
+        let ScoredCoord { coord, .. } = self.open_set_queue.pop()?;
+
+        self.open_set.remove(&coord);
+        self.closed_set.insert(coord);
+
+        Some(coord)
+    }
+
+    fn discover(&mut self, node: T::Coord, tentative_gscore: u32) -> bool {
+        if !self.open_set.insert(node) {
+            return false;
+        }
+
+        let fscore = tentative_gscore.saturating_add(self.graph.heuristic(node));
+        self.open_set_queue.push(scored(node, fscore));
+        true
+    }
+
+    fn g_score(&self, node: &T::Coord) -> u32 {
+        self.g_score.get(node).cloned().unwrap_or(std::u32::MAX)
+    }
+
+    fn reconstruct_path(&self, start: T::Coord) -> Vec<T::Coord> {
+        let mut path = vec![start];
+        let mut current = start;
+
+        while self.came_from.contains_key(&current) {
+            current = self.came_from[&current];
+            path.push(current);
+        }
+
+        path.reverse();
+        path
+    }
+}
+
+struct ScoredCoord<C> {
+    coord: C,
+    score: u32,
+}
+
+impl<C> Ord for ScoredCoord<C> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // inverted on purpose, since we want the coordinates with the lowest f-score first
+        other.score.cmp(&self.score)
+    }
+}
+
+impl<C> PartialOrd for ScoredCoord<C> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<C> PartialEq for ScoredCoord<C> {
+    fn eq(&self, other: &Self) -> bool {
+        self.score == other.score
+    }
+}
+
+impl<C> Eq for ScoredCoord<C> {}
+
+fn scored<C>(coord: C, score: u32) -> ScoredCoord<C> {
+    ScoredCoord { coord, score }
+}
